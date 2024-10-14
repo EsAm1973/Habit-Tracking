be070a865b8b6252edb59a538d74557d9e4f1fe8
Abdelrahman Sobhy <bodeysobhy@gmail.com>
2024-10-14 06:19:40 +0300

    first page of profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_tracking/screens/login_screen.dart';
+import 'package:habit_tracking/screens/profile_screen.dart';
 import 'package:habit_tracking/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
@@ -30,7 +31,7 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginScreen(),
+        home: ProfileScreen(),
       ),
     );
   }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+import '../widgets/custom_photo_container.dart';
+import '../widgets/log_out_container.dart';
+import '../widgets/more_information_container.dart';
+import '../widgets/my_account_container.dart';
+
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:
+      CustomScrollView(
+        scrollDirection: Axis.vertical,
+        slivers: [
+          SliverAppBar(
+            leading: const SizedBox(),
+            snap: false,
+            pinned: false,
+            floating: false,
+            stretch: true,
+            flexibleSpace: Stack(
+              children: [
+                FlexibleSpaceBar(
+                  background:
+                Container(
+                   color: Colors.deepPurple,
+                 ),
+                ),
+                const CustomPhotoContainer(),
+              ],
+            ),
+            expandedHeight: 200,
+          ),
+           const SliverToBoxAdapter(
+            child: Padding(
+              padding:
+              EdgeInsets.symmetric(vertical: 8.0, horizontal: 24),
+              child: Column(
+                    children: [
+                      Text(
+                            'Ahmed',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                            'ahmed@gmail.com',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      MyAccountContainer(),
+                      MoreInformationContainer(),
+                      LogOutContainer(),
+                    ],
+                  ))
+            ),
+        ],
+      ),
+    );
   }
 }
