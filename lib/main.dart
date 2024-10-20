64b1f73b5ab04464a64baec1dff3e0db85dc7ce1
EsAm1973 <me354172@gmail.com>
2024-10-20 22:31:52 +0300

    Make a Notification Service

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracking/screens/login_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:habit_tracking/screens/splash_screen.dart';
 import 'package:habit_tracking/services/auth_service.dart';
+import 'package:habit_tracking/services/notification_service.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NotificationService.initializeNotification();
+  await NotificationService.showDailyNotificationAt1PM();
+
   runApp(const MyApp());
 }
 
@@ -25,13 +29,15 @@ class _MyAppState extends State<MyApp> {
       providers: [
         Provider<AuthService>(create: (_) => AuthService()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Habit Tracker',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      child: ScreenUtilInit(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Habit Tracker',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashScreen(),
         ),
-        home: SplashScreen(),
       ),
     );
   }
